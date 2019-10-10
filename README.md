c23c277268629aae87ac0aa07edb18c47f2810d7
holofish <holofish@gmail.com>
2019-10-10 10:44:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qytzvl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qytzvl)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { HttpClientModule }    from '@angular/common/http';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent ],
+  providers: [],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
